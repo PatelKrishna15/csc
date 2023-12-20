10c61dbd3557fcba89ec4b5f5bb37044697be7e2
Nihir Zala <zalanihir8866@gmail.com>
2023-12-20 14:34:59 +0530

    update the publisher files

--- src/app/Console/Commands/PublishPackageResources.php
+++ src/app/Console/Commands/PublishPackageResources.php
@@ -12,18 +12,18 @@ class PublishPackageResources extends Command
     public function handle()
     {
         // Get package base path
-        $packagePath = dirname(__DIR__, 2);
+        // $packagePath = dirname(__DIR__, 2);
 
         // Publish files and directories
         $this->publishes([
-            $packagePath . '/DataProviders' => base_path('app/DataProviders'),
-            $packagePath . '/DataProviders' => base_path('app/DataProviders'),
-            $packagePath . '/Models/Country.php' => app_path('Models/Country.php'),
-            $packagePath . '/Models/State.php' => app_path('Models/State.php'),
-            $packagePath . '/Models/City.php' => app_path('Models/City.php'),
-            $packagePath . '/database/seeders/CountryStateCityTableSeeder.php' => base_path('database/seeders/CountryStateCityTableSeeder.php'),
-            $packagePath . '/database/migrations/2023_12_14_000000_create_country_state_city_table.php' => base_path('database/migrations/2023_12_14_000000_create_country_state_city_table.php'),
-            $packagePath . '/database/migrations/2023_12_18_161918_create_table_country_extra_table.php' => base_path('database/migrations/2023_12_18_161918_create_table_country_extra_table.php'),
+            __DIR__.'/DataProviders' => base_path('app/DataProviders'),
+            __DIR__.'/DataProviders' => base_path('app/DataProviders'),
+            __DIR__.'/Models/Country.php' => app_path('Models/Country.php'),
+            __DIR__.'/Models/State.php' => app_path('Models/State.php'),
+            __DIR__.'/Models/City.php' => app_path('Models/City.php'),
+            __DIR__.'/database/seeders/CountryStateCityTableSeeder.php' => base_path('database/seeders/CountryStateCityTableSeeder.php'),
+            __DIR__.'/database/migrations/2023_12_14_000000_create_country_state_city_table.php' => base_path('database/migrations/2023_12_14_000000_create_country_state_city_table.php'),
+            __DIR__.'/database/migrations/2023_12_18_161918_create_table_country_extra_table.php' => base_path('database/migrations/2023_12_18_161918_create_table_country_extra_table.php'),
         ], 'csc');
 
         $this->info('Package resources published successfully.');
